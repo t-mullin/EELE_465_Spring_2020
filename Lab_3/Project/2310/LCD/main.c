4307871b75ecd036b075a7383d458f488abc07e2
Tristan <tristan.mullin@student.montana.edu>
2020-02-27 08:04:21 -0700

    working LCD code, need to add some line/screen checking

--- Lab_3/Project/2310/LCD/main.c
+++ Lab_3/Project/2310/LCD/main.c
@@ -6,13 +6,17 @@
 char data_in = 'x';
 
 void delay(unsigned int i) {
-    //TB0CCR0 = i;
-    //TB0CTL |= TBCLR;
-    //while(TB0R != TB0CCR0) {}
-    //P2OUT ^= BIT0;
     unsigned int j;
-    for(j = 0; j < 10000; j=j+1) {}
-    P2OUT ^= BIT0;
+    //1600 is ~15.33 ms
+    //1700 is ~16 ms
+    //400 is ~4 ms
+    //450 is ~4.3 ms
+    //500 is ~4.8 ms
+    //10 is ~130 us
+    //5 is ~84.5 us
+    //2 is ~53.2 us
+    //1 is ~44.6 us
+    for(j = 0; j < i; j=j+1) {}
 }
 
 void toggleEnable() {
@@ -20,79 +24,50 @@ void toggleEnable() {
     P2OUT &= ~BIT0;
 }
 
-void writeToLCD(int nibble, int delayNum) {
+void writeToLCD(int nibble, unsigned int delayNum) {
     P1OUT = nibble;
     toggleEnable();
     delay(delayNum);
 }
 
-
 void init_LCD() {
-
-    //2 is ~75us
-    // 4 is ~150us
-    // 135 is ~4.15ms
-    // 525 is ~16ms
-
-    delay(525); // Start up delay
-
+    //after flashing, remove the debug RX and TX pins
+    //they were causing the lcd to not display correctly
+    delay(1700); // Start up delay
     //function set 8-bit, delay > 4.1ms
-    writeToLCD(0x30, 135);
+    writeToLCD(0x30, 500);
+    //P2OUT |= BIT0;
+    //P1OUT = 0x30;
+    //P2OUT &= ~BIT0;
+    //delay(500);
     //function set 8-bit, delay > 150us
-    writeToLCD(0x30, 4);
+    writeToLCD(0x30, 10);
+    //P2OUT |= BIT0;
+    //P1OUT = 0x30;
+    //P2OUT &= ~BIT0;
+    //delay(10);
     //function set, delay 8-bit, > 40us
-    writeToLCD(0x30, 4);
+    writeToLCD(0x30, 2);
 
     //function set 4-bit, delay > 40us
-    writeToLCD(0x30, 4);
-
+    writeToLCD(0x20, 2);
     //function set 4-bit
-    writeToLCD(0x20, 0);
-    writeToLCD(0x80, 0); //NF**, N = # of lines, F = Char font
-
-    //Display off
-    writeToLCD(0x00, 0);
-    writeToLCD(0x10, 0); //1DCB, D = toggle display on/off, C = cursor, B = blink
-
-    //Entry Mode Set
-    writeToLCD(0x00, 0);
-    writeToLCD(0x60, 0); //01I/DS, I/D = increment/decrement, S = shifts the display when 1
+    writeToLCD(0x20, 2);
+    writeToLCD(0x80, 2); //NF**, N = # of lines, F = Char font
 
     //Display on
-    writeToLCD(0x00, 0);
-    writeToLCD(0xE0, 0); //1DCB, D = toggle display on/off, C = cursor, B = blink
-    /*
-    //function set 4bit
-    P1OUT = 0x20;
-    toggleEnable();
-    delay(8);     // 135 is ~4.15ms
+    writeToLCD(0x00, 2);
+    writeToLCD(0xF0, 2); //1DCB, D = toggle display on/off, C = cursor, B = blink
 
-    //function set
-    P1OUT = 0x20;
-    writeToLCD();
-    P1OUT = 0x80;
-    writeToLCD();
+    //Clear display
+    writeToLCD(0x00, 2);
+    delay(170);
+    writeToLCD(0x10, 2);
+    delay(170);
 
-    //display on
-    P1OUT = 0x00;
-    writeToLCD();
-    P1OUT = 0xE0;
-    writeToLCD();
-
-    //display clear
-    //P1OUT = 0x00;
-    //writeToLCD();
-    //delay(110);
-    //P1OUT = 0x10;
-    //writeToLCD();
-    //delay(110);
-
-    //Entry mode set
-    P1OUT = 0x00;
-    writeToLCD();
-    P1OUT = 0x60;
-    writeToLCD();
-*/
+    //Entry Mode Set
+    writeToLCD(0x00, 2);
+    writeToLCD(0x60, 2); //01I/DS, I/D = increment/decrement, S = shifts the display when 1
 }
 
 int main(void)
@@ -108,23 +83,19 @@ int main(void)
     UCB0I2COA0 |= UCGCEN;
     UCB0I2COA0 |= UCOAEN;
 
-    //-- 2.1 - Configure Timers
-    //TB0CTL |= TBCLR;
-    //TB0CTL |= TBSSEL__ACLK;
-    //TB0CTL |= MC__UP;
-
     //-- 3. Configure Pins
     P1SEL1 &= ~BIT3;        //P1.3 = SCL
     P1SEL0 |= BIT3;
     P1SEL1 &= ~BIT2;        //P1.2 = SDA
     P1SEL0 |= BIT2;
     //-- 3.1
-    P1DIR |= BIT7|BIT6|BIT5|BIT4;
-    P2DIR |= BIT7|BIT6|BIT0;
 
-    P1OUT &= ~BIT7|~BIT6|~BIT5|~BIT4;
-    //rs 2.6, en 2.0
-    P2OUT &= ~BIT6|~BIT0; //Clear RS and enable;
+    P1DIR |= BIT7|BIT6|BIT5|BIT4|BIT0;
+    P2DIR |= BIT6|BIT0;
+
+    P1OUT &= ~BIT7|~BIT6|~BIT5|~BIT4|~BIT0;
+    // r/w 1.0, rs 2.6, en 2.0
+    P2OUT &= ~BIT6|~BIT0; //Clear R/W, RS, and enable;
 
     PM5CTL0 &= ~LOCKLPM5;   //Disable LPM
     //-- 4. Take eUSCI_B0 out of SW reset
@@ -133,48 +104,158 @@ int main(void)
     UCB0IE |= UCRXIE0;
     __enable_interrupt();
 
-    //init_LCD();
+    init_LCD();
 
     while(1) {
-        delay(1);
-        /*
+        //need to check if near the end of a line and/or if the display is full
         switch(data_in) {
             case '0':
+                P2OUT |= BIT6;
+                P1OUT |= BIT0;
+                writeToLCD(0x30, 2);
+                P1OUT &= ~BIT0;
+                writeToLCD(0x00, 2);
+                P2OUT &= ~BIT6;
+                data_in = 'x';
                 break;
             case '1':
-                delay(1000);
+                P2OUT |= BIT6;
+                P1OUT |= BIT0;
+                writeToLCD(0x30, 2);
+                P1OUT &= ~BIT0;
+                writeToLCD(0x10, 2);
+                P2OUT &= ~BIT6;
+                data_in = 'x';
                 break;
             case '2':
+                P2OUT |= BIT6;
+                P1OUT |= BIT0;
+                writeToLCD(0x30, 2);
+                P1OUT &= ~BIT0;
+                writeToLCD(0x20, 2);
+                P2OUT &= ~BIT6;
+                data_in = 'x';
                 break;
             case '3':
+                P2OUT |= BIT6;
+                P1OUT |= BIT0;
+                writeToLCD(0x30, 2);
+                P1OUT &= ~BIT0;
+                writeToLCD(0x30, 2);
+                P2OUT &= ~BIT6;
+                data_in = 'x';
                 break;
             case '4':
+                P2OUT |= BIT6;
+                P1OUT |= BIT0;
+                writeToLCD(0x30, 2);
+                P1OUT &= ~BIT0;
+                writeToLCD(0x40, 2);
+                P2OUT &= ~BIT6;
+                data_in = 'x';
                 break;
             case '5':
+                P2OUT |= BIT6;
+                P1OUT |= BIT0;
+                writeToLCD(0x30, 2);
+                P1OUT &= ~BIT0;
+                writeToLCD(0x50, 2);
+                P2OUT &= ~BIT6;
+                data_in = 'x';
                 break;
             case '6':
+                P2OUT |= BIT6;
+                P1OUT |= BIT0;
+                writeToLCD(0x30, 2);
+                P1OUT &= ~BIT0;
+                writeToLCD(0x60, 2);
+                P2OUT &= ~BIT6;
+                data_in = 'x';
                 break;
             case '7':
+                P2OUT |= BIT6;
+                P1OUT |= BIT0;
+                writeToLCD(0x30, 2);
+                P1OUT &= ~BIT0;
+                writeToLCD(0x70, 2);
+                P2OUT &= ~BIT6;
+                data_in = 'x';
                 break;
             case '8':
+                P2OUT |= BIT6;
+                P1OUT |= BIT0;
+                writeToLCD(0x30, 2);
+                P1OUT &= ~BIT0;
+                writeToLCD(0x80, 2);
+                P2OUT &= ~BIT6;
+                data_in = 'x';
                 break;
             case '9':
+                P2OUT |= BIT6;
+                P1OUT |= BIT0;
+                writeToLCD(0x30, 2);
+                P1OUT &= ~BIT0;
+                writeToLCD(0x90, 2);
+                P2OUT &= ~BIT6;
+                data_in = 'x';
                 break;
             case 'A':
+                P2OUT |= BIT6;
+                P1OUT |= BIT0;
+                writeToLCD(0x40, 2);
+                P1OUT &= ~BIT0;
+                writeToLCD(0x10, 2);
+                P2OUT &= ~BIT6;
+                data_in = 'x';
                 break;
             case 'B':
+                P2OUT |= BIT6;
+                P1OUT |= BIT0;
+                writeToLCD(0x40, 2);
+                P1OUT &= ~BIT0;
+                writeToLCD(0x20, 2);
+                P2OUT &= ~BIT6;
+                data_in = 'x';
                 break;
             case 'C':
+                P2OUT |= BIT6;
+                P1OUT |= BIT0;
+                writeToLCD(0x40, 2);
+                P1OUT &= ~BIT0;
+                writeToLCD(0x30, 2);
+                P2OUT &= ~BIT6;
+                data_in = 'x';
                 break;
             case 'D':
+                P2OUT |= BIT6;
+                P1OUT |= BIT0;
+                writeToLCD(0x40, 2);
+                P1OUT &= ~BIT0;
+                writeToLCD(0x40, 2);
+                P2OUT &= ~BIT6;
+                data_in = 'x';
                 break;
             case '*':
+                P2OUT |= BIT6;
+                P1OUT |= BIT0;
+                writeToLCD(0x40, 2);
+                P1OUT &= ~BIT0;
+                writeToLCD(0x50, 2);
+                P2OUT &= ~BIT6;
+                data_in = 'x';
                 break;
             case '#':
+                P2OUT |= BIT6;
+                P1OUT |= BIT0;
+                writeToLCD(0x40, 2);
+                P1OUT &= ~BIT0;
+                writeToLCD(0x60, 2);
+                P2OUT &= ~BIT6;
+                data_in = 'x';
                 break;
             default:
                 break;
-        }*/
+        }
     }
     return 0;
 }
