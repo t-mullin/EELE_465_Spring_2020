447ae1d10651cfebede2ef1b94796180a31966ba
Tristan <tristan.mullin@student.montana.edu>
2020-03-16 17:41:17 -0600

    modified code to display enter message and outline of tempature text

--- Lab_5/Project/2310/LCD/main.c
+++ Lab_5/Project/2310/LCD/main.c
@@ -1,12 +1,206 @@
+//Lizzy Hamaoka & Tristan Mullin
+//3/17/2020
+//Lab 4: Setting up RS-232 communication between a computer and master micro-controller
 #include <msp430.h> 
-
-
 /**
  * main.c
  */
+char data_in = 'x';
+unsigned int position = 0;
+int data_ready = 0;
+char enterMessage[] = "Enter n:";
+char LM19Message[]  = " T,K:kk  T,C:cc ";
+char MSP430Message[]  = " T,K:kk  T,C:cc ";
+
+
+void delay(unsigned int i) {
+    unsigned int j;
+    //1700 is ~16 ms
+    //500 is ~4.8 ms
+    //10 is ~130 us
+    //2 is ~53.2 us
+    for(j = 0; j < i; j=j+1) {}
+}
+
+//Writes the nibble to the lcd
+void writeToLCD(int nibble, unsigned int delayNum) {
+    P1OUT = nibble;
+    P2OUT |= BIT0;
+    P2OUT &= ~BIT0;
+    delay(delayNum);
+}
+
+//software initialization of the LCD
+void init_LCD() {
+    //after flashing, remove the debug RX and TX pins
+    //they were causing the lcd to not display correctly
+    delay(1700); // Start up delay
+    //function set 8-bit, delay > 4.1ms
+    writeToLCD(0x30, 500);
+    //function set 8-bit, delay > 150us
+    writeToLCD(0x30, 10);
+    //function set, delay 8-bit, > 40us
+    writeToLCD(0x30, 2);
+
+    //function set 4-bit, delay > 40us
+    writeToLCD(0x20, 2);
+    //function set 4-bit
+    writeToLCD(0x20, 2);
+    writeToLCD(0x80, 2); //NF**, N = # of lines, F = Char font
+
+    //Display on
+    writeToLCD(0x00, 2);
+    writeToLCD(0xF0, 2); //1DCB, D = toggle display on/off, C = cursor, B = blink
+
+    //Clear display
+    writeToLCD(0x00, 2);
+    delay(170);
+    writeToLCD(0x10, 2);
+    delay(170);
+
+    //Entry Mode Set
+    writeToLCD(0x00, 2);
+    writeToLCD(0x60, 2); //01I/DS, I/D = increment/decrement, S = shifts the display when 1
+}
+
+//sets the rs and rw depending on what needs to be written or read
+void setRS_RW(unsigned int rs, unsigned int rw) {
+    if (rs == 1) {
+        P2OUT |= BIT6;
+    } else {
+        P2OUT &= ~BIT6;
+    }
+    if(rw == 1) {
+        P1OUT |= BIT0;
+    } else {
+        P1OUT &= ~BIT0;
+    }
+}
+
+//checks to see if the cursor has reached the end of the screen then wraps or clears the screen
+void checkEndOfScreen() {
+    setRS_RW(0, 0);
+    if(position == 16) {
+        writeToLCD(0xC0, 2);
+        writeToLCD(0x00, 2);
+    } else if (position == 33) {
+        writeToLCD(0x00, 2);
+        writeToLCD(0x10, 2);
+        position = 0;
+    }
+}
+
+//Writes the character to the LCD screen
+void writeChar(char input) {
+    setRS_RW(1, 1);
+    writeToLCD(input, 2);
+    setRS_RW(1, 0);
+    input = input << 4;
+    writeToLCD(input, 2);
+    setRS_RW(0, 0);
+    position++;
+}
+
+void writeDefault() {
+    int i;
+    for(i = 0; i < sizeof(enterMessage)-1; i++) {
+        writeChar(enterMessage[i]);
+    }
+}
+
+void writeTemperature() {
+    int i;
+    for(i = 0; i < sizeof(LM19Message)-1; i++) {
+        writeChar(LM19Message[i]);
+    }
+    checkEndOfScreen();
+    for(i = 0; i < sizeof(MSP430Message)-1; i++) {
+        writeChar(MSP430Message[i]);
+    }
+}
+
+void clearScreen() {
+    setRS_RW(0, 0);
+    writeToLCD(0x00, 2);
+    delay(170);
+    writeToLCD(0x10, 2);
+    delay(170);
+    position = 0;
+}
+
 int main(void)
 {
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	
-	return 0;
+    WDTCTL = WDTPW | WDTHOLD;
+    //-- 1. Put eUSCI_B0 into software reset
+    UCB0CTLW0 |= UCSWRST;   //UCSWRST=1 for eUSCI_B0 in SW reset
+    //-- 2. Configure eUSCI_B0
+    UCB0CTLW0 |= UCMODE_3;  //Put into I2C mode
+    UCB0I2COA0 = 0x0043;     //Slave address = 0x42
+    UCB0CTLW0 &= ~UCMST;     //Put into slave mode
+    UCB0CTLW0 &= ~UCTR;      //Put into Rx mode
+    UCB0I2COA0 |= UCGCEN;
+    UCB0I2COA0 |= UCOAEN;
+    //-- 3. Configure Pins
+    P1SEL1 &= ~BIT3;        //P1.3 = SCL
+    P1SEL0 |= BIT3;
+    P1SEL1 &= ~BIT2;        //P1.2 = SDA
+    P1SEL0 |= BIT2;
+    //-- 3.1
+    P1DIR |= BIT7|BIT6|BIT5|BIT4|BIT0;
+    P2DIR |= BIT6|BIT0;
+    P1OUT &= ~BIT7|~BIT6|~BIT5|~BIT4|~BIT0;
+    // r/w 1.0, rs 2.6, en 2.0
+    P2OUT &= ~BIT6|~BIT0; //Clear R/W, RS, and enable;
+    PM5CTL0 &= ~LOCKLPM5;   //Disable LPM
+    //-- 4. Take eUSCI_B0 out of SW reset
+    UCB0CTLW0 &= ~UCSWRST;  //UCSWRST=1 for eUSCI_B0 in SW reset
+    //-- 5. Enable Interrupts
+    UCB0IE |= UCRXIE0;
+    __enable_interrupt();
+
+    init_LCD();
+
+    writeDefault();
+
+    while(1) {
+        //checkEndOfScreen();
+        //checks to see if there has been any data has been recived
+        if(data_ready == 1) {
+            switch(data_in) {
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    writeChar(data_in);
+                    delay(50000);
+                    clearScreen();
+                    writeTemperature();
+                    data_ready = 0;
+                    break;
+                case '*':
+                    clearScreen();
+                    writeDefault();
+                    data_ready = 0;
+                    break;
+            }
+        }
+    }
+    return 0;
+}
+//------- Interrupt Service Routines ---------------------------
+#pragma vector=USCI_B0_VECTOR
+__interrupt void USCI_B0_ISR(void) {
+    switch(UCB0IV) {
+        case 0x16:
+            data_in = UCB0RXBUF;
+            data_ready = 1;
+            break;
+        default:
+            break;
+    }
 }
--- Lab_5/Project/2355/main.c
+++ Lab_5/Project/2355/main.c
@@ -1,12 +1,195 @@
+//Lizzy Hamaoka & Tristan Mullin
+//3/17/2020
+//Lab 4: Setting up RS-232 communication between a computer and master micro-controller
 #include <msp430.h> 
-
-
 /**
  * main.c
  */
+char input;
+int data_ready = 0;
+
+void delay(int delayNum) {
+    int i;
+    for(i = 0; i < delayNum; i++) {}
+}
+
 int main(void)
 {
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	
-	return 0;
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+
+    //-- 1.A Put eUSCI_B0 into software reset
+    UCB0CTLW0 |= UCSWRST;   //UCSWRST=1 for eUSCI_B0 in SW reset
+    //-- 1.B Put eUSCI_A0 into software reset
+    UCA0CTLW0 |= UCSWRST;   //UCSWRST=1 for eUSCI_B0 in SW reset
+
+    //-- 2.A Configure eUSCI_B0
+    UCB0CTLW0 |= UCSSEL_3;  //Choose BRCLK=SMCLK=1MHz
+    UCB0BRW = 10;           //Divide BRCLK by 10 for SCL=100kHz
+    UCB0CTLW0 |= UCMODE_3;  //Put into I2C mode
+    UCB0CTLW0 |= UCMST;     //Put into master mode
+    UCB0CTLW0 |= UCTR;      //Put into Tx mode
+    UCB0I2CSA = 0x0042;     //Initial Slave address = 0x42
+    UCB0CTLW1 |= UCASTP_2;  //Auto STOP when UCB0TBCNT reached
+    UCB0TBCNT = 0x01;       //Send one byte of data
+
+    //-- 2.B Configure eUSCI_A0
+    UCA0CTLW0 |= UCSSEL__ACLK;  //Choose BRCLK=ACLK=32.768kHz
+    UCA0BRW = 3;
+    UCA0MCTLW |= 0x9200;
+
+    //-- 2.C Initialize Temp Sensor
+    PMMCTL2 |= TSENSOREN;
+
+
+    //-- 3.A Configure Pins
+    P1SEL1 &= ~BIT3;        //P1.3 = SCL
+    P1SEL0 |= BIT3;
+    P1SEL1 &= ~BIT2;        //P1.2 = SDA
+    P1SEL0 |= BIT2;
+
+    //-- 3.B
+    P1SEL1 &= ~BIT6;        //P1.6 = RX
+    P1SEL0 |= BIT6;
+    P1SEL1 &= ~BIT7;        //P1.7 = TX
+    P1SEL0 |= BIT7;
+
+    //-- 3.C
+    P2DIR = 0xF0;           //Upper 4 bits set as outputs, Lower 4 bits set as inputs
+    P2OUT = 0x0F;
+    P2REN = 0x0F;
+
+
+    PM5CTL0 &= ~LOCKLPM5;   //Disable LPM
+
+    //-- 4.A Take eUSCI_B0 out of SW reset
+    UCB0CTLW0 &= ~UCSWRST;  //UCSWRST=1 for eUSCI_B0 in SW reset
+    //-- 4.B Take eUSCI_A0 out of SW reset
+    UCA0CTLW0 &= ~UCSWRST;  //UCSWRST=1 for eUSCI_A0 in SW reset
+
+    //-- 5. Enable Interrupts
+    UCB0IE |= UCTXIE0;      //Enable I2C Tx0 IRQ
+    UCA0IE |= UCRXIE;       //Enable UART RX IRQ
+    P2IFG = 0x00;
+    P2IES = 0x0F;
+    P2IE = 0x0F;
+    __enable_interrupt();   //Enable Maskable IRQs
+
+    while(1){
+        if(data_ready == 1) {
+            UCB0I2CSA = 0x0042;     //Slave address = 0x42 LED
+            UCB0CTLW0 |= UCTXSTT;
+            delay(100);
+            //for(i=0; i<100; i++){}
+            UCB0I2CSA = 0x0043;     //Slave address = 0x43 LCD
+            UCB0CTLW0 |= UCTXSTT;
+            delay(100);
+            //for(i=0; i<100; i++){}
+            UCA0TXBUF = input;      //sending data back to terminal
+            delay(10000);
+            data_ready = 0;
+        }
+    }
+    return 0;
+}
+//------- Interrupt Service Routines ---------------------------
+#pragma vector=USCI_B0_VECTOR
+__interrupt void USCI_B0_ISR(void) {
+    switch (UCB0IV) {
+        case 0x18: //Vector 24 TXIFG0
+            UCB0TXBUF = input;
+            break;
+        default:
+            break;
+    }
+}
+
+#pragma vector=USCI_A0_VECTOR
+__interrupt void USCI_A0_RX_ISR(void) {
+    input = UCA0RXBUF;
+    data_ready = 1;
+}
+
+#pragma vector=PORT2_VECTOR
+__interrupt void PORT2_ISR(void){
+    switch(P2IV){
+        case 0x02:
+            P2DIR = 0x0F;
+            P2OUT = 0xF0;
+            P2REN = 0xF0;
+            if(P2IN == 0x70) {
+                input = 'A'; //A
+                data_ready = 1;
+            } else if (P2IN == 0xB0) {
+                input = 'B'; //B
+                data_ready = 1;
+            } else if (P2IN == 0xD0) {
+                input = 'C'; //C
+                data_ready = 1;
+            } else if (P2IN == 0xE0) {
+                input = 'D'; //D
+                data_ready = 1;
+            }
+            break;
+        case 0x04:
+            P2DIR = 0x0F;
+            P2OUT = 0xF0;
+            P2REN = 0xF0;
+            if(P2IN == 0x70) {
+                input = '3'; //3
+                data_ready = 1;
+            } else if (P2IN == 0xB0) {
+                input = '6'; //6
+                data_ready = 1;
+            } else if (P2IN == 0xD0) {
+                input = '9'; //9
+                data_ready = 1;
+            } else if (P2IN == 0xE0) {
+                input = '#'; //#
+                data_ready = 1;
+            }
+            break;
+        case 0x06:
+            P2DIR = 0x0F;
+            P2OUT = 0xF0;
+            P2REN = 0xF0;
+            if(P2IN == 0x70) {
+                input = '2'; //2
+                data_ready = 1;
+            } else if (P2IN == 0xB0) {
+                input = '5'; //5
+                data_ready = 1;
+            } else if (P2IN == 0xD0) {
+                input = '8'; //8
+                data_ready = 1;
+            } else if (P2IN == 0xE0) {
+                input = '0'; //0
+                data_ready = 1;
+            }
+            break;
+        case 0x08:
+            P2DIR = 0x0F;
+            P2OUT = 0xF0;
+            P2REN = 0xF0;
+            if(P2IN == 0x70) {
+                input = '1'; //1
+                data_ready = 1;
+            } else if (P2IN == 0xB0) {
+                input = '4'; //4
+                data_ready = 1;
+            } else if (P2IN == 0xD0) {
+                input = '7'; //7
+                data_ready = 1;
+            } else if (P2IN == 0xE0) {
+                input = '*'; //'*'
+                data_ready = 1;
+            }
+            break;
+        default:
+            break;
+    }
+    //resets the direction, input mask, and resistors
+    P2DIR = 0xF0;           //Upper 4 bits set as outputs, Lower 4 bits set as inputs
+    P2OUT = 0x0F;
+    P2REN = 0x0F;
+    P2IFG = 0x00;   //clear the interrupt flag
 }
